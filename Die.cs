7b0857ee033f48d0f4cdbf640d208e9a4ca84b79
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-18 14:03:25 -0500

    Start Tutorial as a group

---
+++ Die.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5___More_Classes
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+    }
+}
